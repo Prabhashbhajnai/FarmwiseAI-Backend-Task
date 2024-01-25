a2fe8ebbb202fdbe87b4b0760357b09c3d0bf58b
Prabhashbhajani <prabhashbhajani@gmail.com>
2024-01-25 15:34:00 +0530

    Authenticatin added for private routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,17 @@
 import jwt from 'jsonwebtoken'
 
 const authenticateJWT = async (req, res, next) => {
-    const token = req.header('authorization')
+    const authHeader = req.header('Authorization')
 
-    if (!token)
-        return res.status(401).json({ success: false, message: "Unauthorized" })
+
+    if (!authHeader)
+        return res.status(401).json({ success: false, message: "Unauthorized: Not Logged In" })
+
+    const token = authHeader.split(' ')[1]
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err)
-            return res.status(403).json({ success: false, message: "Forbidden" })
+            return res.status(403).json({ success: false, message: "Forbidden: Invalid Token" })
 
         req.user = user
         next()
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+// Authentication middleware
+import authenticateJWT from "../middleware/auth.js";
+
 // controllers
 import { addBook, getAllBooks } from "../controllers/book.js";
 
 const bookRouter = Router();
 
 bookRouter.get('/', getAllBooks)
-bookRouter.post('/addbook', addBook)
+bookRouter.post('/addbook', authenticateJWT, addBook)
 
 export default bookRouter;
