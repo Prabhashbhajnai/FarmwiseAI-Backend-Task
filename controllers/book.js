5817f0db9c058809649611e91323380bb9a5f7d2
Prabhashbhajani <prabhashbhajani@gmail.com>
2024-01-25 15:52:32 +0530

    API to search book by isbn added

--- controllers/book.js
+++ controllers/book.js
@@ -1,7 +1,12 @@
 // Model
 import Book from "../models/Book.js";
 
-// Get all books
+/* 
+    Route:          /
+    Description:    Get All Books
+    Access:         Public
+    Method :        GET
+*/
 export const getAllBooks = async (req, res) => {
     try {
         const books = await Book.find()
@@ -12,11 +17,39 @@ export const getAllBooks = async (req, res) => {
     }
 }
 
+/* 
+    Route:          /addbook
+    Description:    Add a new book
+    Access:         Private
+    Method :        POST
+*/
 export const addBook = async (req, res) => {
     try {
         const newBook = await Book.create(req.body)
 
         return res.status(201).json({ success: true, result: newBook })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/* 
+    Route:          /:ISBN
+    Description:    Search Book by ISBN
+    Params:         ISBN
+    Access:         Public
+    Method :        GET
+*/
+export const searchByISBN = async (req, res) => {
+    try {
+        const { ISBN } = req.params;
+
+        const book = await Book.findOne({ ISBN: ISBN })
+
+        if (!book)
+            return res.status(404).json({ success: false, message: 'Book not found' })
+        
+        return res.status(200).json({ success: true, result: book })
     } catch (error) {
         console.log(error);
     }
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -4,11 +4,12 @@ import { Router } from "express";
 import authenticateJWT from "../middleware/auth.js";
 
 // controllers
-import { addBook, getAllBooks } from "../controllers/book.js";
+import { addBook, getAllBooks, searchByISBN } from "../controllers/book.js";
 
 const bookRouter = Router();
 
 bookRouter.get('/', getAllBooks)
 bookRouter.post('/addbook', authenticateJWT, addBook)
+bookRouter.get('/:ISBN', searchByISBN)
 
 export default bookRouter;
